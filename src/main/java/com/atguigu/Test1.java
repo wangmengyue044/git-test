b280b4247e8307c37462903fbd21833c2f61993b
Casey <wangmengyue044@163.com>
2023-06-01 20:04:31 +0800

    second commit

--- src/main/java/com/atguigu/Test1.java
+++ src/main/java/com/atguigu/Test1.java
@@ -7,5 +7,8 @@ package com.atguigu;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!!");
+
     }
 }
