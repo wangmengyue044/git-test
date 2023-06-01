0231572ae631bde80a9bb3e13bad9460ebeef683
Casey <wangmengyue044@163.com>
2023-06-01 20:02:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/Test1.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author WangMengyue
+ * @create 2023-06-01 19:56
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
